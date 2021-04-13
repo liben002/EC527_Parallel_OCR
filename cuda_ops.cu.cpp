dbec846f17ae0d3d67c759bba3b09d5d851bc6a0
liben002 <liben002@bu.edu>
2021-04-13 02:41:13 +0100

    Fixing

---
+++ cuda_ops.cu.cpp
@@ -0,0 +1,10 @@
+template <typename T>
+__global__ void CUDA_MAT_SUBT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
+{
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
+	int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (row < row_len && col < col_len)
+		d_C[row*row_len+col] = d_A[row*row_len+col] - d_B[row*row_len+col];
+
+}
