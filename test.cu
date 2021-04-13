0edbef05cfcd5baa9d5d7843c29e3de34a600242
liben002 <liben002@bu.edu>
2021-04-13 13:47:55 +0100

    Fixing

--- test.cu
+++ test.cu
@@ -12,7 +12,7 @@ __global__ void CUDA_MAT_SUBT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
 	int row = blockIdx.x * blockDim.x + threadIdx.x;
 	int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-	if (row < row_len && col < col_len)
+	if (row <= row_len && col <= col_len)
 		d_C[row*col_len+col] = d_A[row*col_len+col] - d_B[row*col_len+col];
 
 }
