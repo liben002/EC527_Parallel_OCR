c2a258fe57032f347ffe19fa0ea05585f8cdf55f
liben002 <liben002@bu.edu>
2021-04-13 02:34:04 +0100

    Fixing

--- vector_ops.hpp
+++ vector_ops.hpp
@@ -17,6 +17,17 @@
 #include <valarray>
 #include <vector>
 
+template <typename T>
+__global__ void CUDA_MAT_SUBT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
+{
+	int row = blockIdx.x * blockDim.x + threadIdx.x;
+	int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if (row < row_len && col < col_len)
+		d_C[row*row_len+col] = d_A[row*row_len+col] - d_B[row*row_len+col];
+
+}
+
 /**
  * @namespace machine_learning
  * @brief Machine Learning algorithms
@@ -458,9 +469,7 @@ std::vector<std::valarray<T>> operator+(const std::vector<std::valarray<T>> &A, 
  * @return new resultant vector
  */
 template <typename T>
-std::vector<std::valarray<T>> operator-(
-	const std::vector<std::valarray<T>> &A,
-	const std::vector<std::valarray<T>> &B) {
+std::vector<std::valarray<T>> operator-(const std::vector<std::valarray<T>> &A, const std::vector<std::valarray<T>> &B) {
 	const auto shape_a = get_shape(A);
 	const auto shape_b = get_shape(B);
 	// If vectors don't have equal shape
@@ -470,10 +479,98 @@ std::vector<std::valarray<T>> operator-(
 		std::exit(EXIT_FAILURE);
 	}
 
-	std::vector<std::valarray<T>> C;         // Vector to store result
-	for (size_t i = 0; i < A.size(); i++) {  // For every row
-		C.push_back(A[i] - B[i]);            // Elementwise substraction
+	// Error code to check return values for CUDA calls
+	cudaError_t err = cudaSuccess;
+
+	// Allocate host memory
+	printf("Allocating host vectors.\n");
+	T *h_A = (T *) malloc(mat_size);
+	T *h_B = (T *) malloc(mat_size);
+	T *h_C = (T *) malloc(mat_size);
+
+	for (int i = 0; i < shape_a.first; i++) {
+		for (int j = 0; j < shape_a.second; j++) {
+			h_A[i*shape_a.first + j] = A[i][j];
+		}
 	}
+
+	for (int i = 0; i < shape_a.first; i++) {
+		for (int j = 0; j < shape_a.second; j++) {
+			h_B[i*shape_a.first + j] = B[i][j];
+		}
+	}
+
+	// Allocate device vector
+	printf("Allocating device vectors.\n");
+	T *d_A = NULL;
+	T *d_B = NULL;
+	T *d_C = NULL;
+
+	err = cudaMalloc((void **) &d_A, mat_size);
+	err = cudaMalloc((void **) &d_B, mat_size);
+	err = cudaMalloc((void **) &d_C, mat_size);
+
+	printf ("Copying host vectors to CUDA device vectors\n");
+	err = cudaMemcpy(d_A, h_A, mat_size, cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_B, h_B, mat_size, cudaMemcpyHostToDevice);
+
+	dim3 dimBlock(8, 8);
+	dim3 dimGrid(4, 4);
+	printf("Launching CUDA kernel with %d blocks and %d threads.\n", 4, 4 * 4);
+
+	CUDA_MAT_SUBT<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, shape_a.first, shape_a.second);
+
+	err = cudaGetLastError();
+
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to launch MMM kernel (error code: %s)!\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Copy output data from CUDA device to the host memory\n");
+	err = cudaMemcpy(h_C, d_C, mat_size, cudaMemcpyDeviceToHost);
+
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to copy matrix from device to host (error code: %s)!\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	std::vector<std::valarray<T> > C(shape_a.first);         // Vector to store result
+	for (size_t i = 0; i < shape_a.first; i++) {  // For every row
+		std::valarray<T> temp(1,shape_a.second);
+		for (size_t j = 0; j < shape_a.second; j++) {
+			temp[j] = h_C[i*shape_a.first + j];
+		}
+		C[i] = temp;            // Elementwise substraction
+	}
+
+	printf("Freeing\n");
+	// Free device global memory
+	err = cudaFree(d_A);
+
+	err = cudaFree(d_B);
+
+	err = cudaFree(d_C);
+
+	// Free host memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
+
+	err = cudaDeviceReset();
+
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to allocate device vector A (error code: %s)!\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	// std::vector<std::valarray<T>> C(shape_a.first);         // Vector to store result
+	// for (size_t i = 0; i < A.size(); i++) {  // For every row
+	// 	C.push_back(A[i] - B[i]);            // Elementwise substraction
+	// }
 
 	return C;  // Return new resultant 2D vector
 }
