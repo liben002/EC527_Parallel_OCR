cd4030f6c96b27226d9c412cc6e1cdb9dd60bc7d
Benjamin Li <pharmashawarma@gmail.com>
2021-04-15 16:07:19 -0400

    Asdf

--- cuda_ops.cu
+++ cuda_ops.cu
@@ -17,16 +17,18 @@ __global__ void CUDA_MAT_MULT(T *d_A, T *d_B, T *d_C, int row_len_dA, int col_le
 	int row = blockIdx.x * blockDim.x + threadIdx.x;
 	int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-	float Pval = 0;
-
 	if (!(row >= row_len_dA || col >= col_len_dA || row >= row_len_dB || col >= col_len_dB))
 	{
-		for (int k = 0; k < row_len; k++) {
-			Pval += d_A[row*row_len_dA+k] * d_B[k*row_len_dB+col];
-			__syncthreads();
-		}
-	}
+		float Pval = 0;
 
-	d_C[row*row_len+col] = Pval;
+		if (!(row >= row_len_dA || col >= col_len_dA || row >= row_len_dB || col >= col_len_dB))
+		{
+			for (int k = 0; k < row_len; k++) {
+				Pval += d_A[row*row_len_dA+k] * d_B[k*row_len_dB+col];
+				__syncthreads();
+			}
+		}
+		d_C[row*row_len+col] = Pval;
+	}
 }
 
