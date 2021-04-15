0fbb11b714e95f18867dd335dd6e8bfce8624072
Benjamin Li <pharmashawarma@gmail.com>
2021-04-15 16:06:20 -0400

    Asdf

--- cuda_ops.cu
+++ cuda_ops.cu
@@ -12,17 +12,19 @@ __global__ void CUDA_MAT_SUBT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
 }
 
 template <typename T>
-__global__ void CUDA_MAT_MULT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
+__global__ void CUDA_MAT_MULT(T *d_A, T *d_B, T *d_C, int row_len_dA, int col_len_dA, int row_len_dB, int col_len_dB)
 {
 	int row = blockIdx.x * blockDim.x + threadIdx.x;
 	int col = blockIdx.y * blockDim.y + threadIdx.y;
 
 	float Pval = 0;
 
-	for (int k = 0; k < row_len; k++) {
-		Pval += d_A[row*row_len+k] * d_B[k*row_len+col];
-		__syncthreads();
-	}
+	if (!(row >= row_len_dA || col >= col_len_dA || row >= row_len_dB || col >= col_len_dB))
+	{
+		for (int k = 0; k < row_len; k++) {
+			Pval += d_A[row*row_len_dA+k] * d_B[k*row_len_dB+col];
+			__syncthreads();
+		}
 
 	d_C[row*row_len+col] = Pval;
 }
