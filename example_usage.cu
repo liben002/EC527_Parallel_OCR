c0bdb1d1526ed3a4f7450ff145c215c30036ecfc
Benjamin Li <pharmashawarma@gmail.com>
2021-04-15 17:42:39 -0400

    asdf

---
+++ example_usage.cu
@@ -0,0 +1,49 @@
+#include "nn.hpp"
+#include <iostream>
+#include <time.h>
+#include <cassert>
+#include <omp.h>
+
+#define EPOCHS 100
+
+/**
+ * Function to test neural network
+ * @returns none
+ */
+static void test() {
+	// Creating network with 3 layers for "iris.csv"
+	// First layer neurons must match testing params
+	machine_learning::neural_network::NeuralNetwork myNN = machine_learning::neural_network::NeuralNetwork({ {4, "none"}, {600, "relu"}, {300, "sigmoid"} });
+
+	// Printing summary of model
+	myNN.summary();
+
+	// Training Model
+	myNN.fit_from_csv("iris.csv", true, EPOCHS, 0.3, false, 2, 32, true);
+
+	printf("Testing predictions\n");
+	// Testing predictions of model
+	assert(machine_learning::argmax(myNN.single_predict({{5, 3.4, 1.6, 0.4}})) == 0);
+	assert(machine_learning::argmax( myNN.single_predict({{6.4, 2.9, 4.3, 1.3}})) == 1);
+	assert(machine_learning::argmax(myNN.single_predict({{6.2, 3.4, 5.4, 2.3}})) == 2);
+	return;
+}
+
+/**
+ * @brief Main function
+ * @returns 0 on exit
+ */
+int main() {
+
+	// start the timer
+	auto start = std::chrono::high_resolution_clock::now();  // Start clock
+
+	test();
+
+	// stop the timer
+	auto stop = std::chrono::high_resolution_clock::now();  // Stoping the clock
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+	printf("Time for learning over %d epochs: %f seconds\n", EPOCHS, duration.count() / 1e6);
+	return 0;
+}
--- vector_ops.hpp
+++ vector_ops.hpp
@@ -590,7 +590,7 @@ std::vector<std::valarray<T>> multiply(const std::vector<std::valarray<T>> &A, c
 	dim3 dimGrid(16, 16);
 	// printf("Launching CUDA kernel with %d blocks and %d threads.\n", 4, 4 * 4);
 
-	CUDA_MAT_MULT<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, shape_a.first, shape_a.second, shape_b.first, shape_b.second);
+	CUDA_MAT_MULT<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, shape_a.first, shape_a.second, shape_b.first, shape_b.second, shape_a.first, shape_b.second);
 
 	err = cudaGetLastError();
 
