942aaea100a1b98b36f0e5cb5e8aa5f8bb054d8c
Benjamin Li <pharmashawarma@gmail.com>
2021-04-15 17:58:20 -0400

    asdf

--- cuda_ops.cu
+++ cuda_ops.cu
@@ -2,30 +2,20 @@
 #include <cuda.h>
 
 template <typename T>
-__global__ void CUDA_MAT_SUBT(T *d_A, T *d_B, T *d_C, int row_len, int col_len)
-{
-	int row = blockIdx.x * blockDim.x + threadIdx.x;
-	int col = blockIdx.y * blockDim.y + threadIdx.y;
-
-	if (row < row_len && col < col_len)
-		d_C[row*col_len+col] = d_A[row*col_len+col] - d_B[row*col_len+col];
-}
-
-template <typename T>
 __global__ void CUDA_MAT_MULT(T *d_A, T *d_B, T *d_C, int A_rows, int A_cols, int B_rows, int B_cols, int C_rows, int C_cols) {
 
-    T c_val = 0;
+	T c_val = 0;
 
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for (int k = 0; k < (A_cols); k++) {
-            if ((k < A_cols && row < A_rows) && (k < B_rows && col < B_cols))
-                c_val += d_A[row * A_cols + k] * d_B[k * B_cols + col];
+	for (int k = 0; k < (A_cols); k++) {
+			if ((k < A_cols && row < A_rows) && (k < B_rows && col < B_cols))
+				c_val += d_A[row * A_cols + k] * d_B[k * B_cols + col];
 
-    }
+	}
 
-    if (row < C_rows && col < C_cols){
-    	d_C[((blockIdx.y * blockDim.y + threadIdx.y)*C_cols)+(blockIdx.x*blockDim.x)+threadIdx.x] = c_val;
-    }
+	if (row < C_rows && col < C_cols){
+		d_C[((blockIdx.y * blockDim.y + threadIdx.y) * C_cols) + (blockIdx.x * blockDim.x) + threadIdx.x] = c_val;
+	}
 }
