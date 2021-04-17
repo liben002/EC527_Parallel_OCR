13af9040bbac7e03c11bae84a59a2b9aae3482dd
Benjamin Li <pharmashawarma@gmail.com>
2021-04-17 15:51:44 -0400

    asdF

--- vector_ops.hpp
+++ vector_ops.hpp
@@ -611,6 +611,7 @@ std::vector<std::valarray<T>> multiply(const std::vector<std::valarray<T>> &A, c
 		CUDA_MAT_MULT_TILED<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, shape_a.first, shape_a.second, shape_b.first, shape_b.second, shape_a.first, shape_b.second, TILE_DIM);
 	#else
 		CUDA_MAT_MULT<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, shape_a.first, shape_a.second, shape_b.first, shape_b.second, shape_a.first, shape_b.second);
+	#endif
 
 	err = cudaGetLastError();
 
