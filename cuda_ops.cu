466caf9c535b25197e37e8e4e81b496f95f16a83
Benjamin Li <pharmashawarma@gmail.com>
2021-04-23 20:02:49 -0400

    testing

--- cuda_ops.cu
+++ cuda_ops.cu
@@ -85,14 +85,14 @@ __global__ void CUDA_MAT_MULT_SHARED(T *d_A, T *d_B, T *d_C, int rows_A, int col
 
 	for (int i = 0; i < (SHARED_TILE_WIDTH + cols_A - 1)/SHARED_TILE_WIDTH; i++) {
 
-		s_A[threadIdx.y][threadIdx.x] = (i * SHARED_TILE_WIDTH + threadIdx.x < cols_A && row < rows_A) ? d_A[row * cols_A + i * SHARED_TILE_WIDTH + threadIdx.x] : 0.0;
+		s_A[threadIdx.x][threadIdx.y] = (i * SHARED_TILE_WIDTH + threadIdx.y < cols_A && row < rows_A) ? d_A[row * cols_A + i * SHARED_TILE_WIDTH + threadIdx.y] : 0.0;
 
-		s_B[threadIdx.y][threadIdx.x] = (i * SHARED_TILE_WIDTH + threadIdx.y < rows_B && col < cols_B) ? d_B[(i * SHARED_TILE_WIDTH + threadIdx.y) * cols_B + col] : 0.0;
+		s_B[threadIdx.x][threadIdx.y] = (i * SHARED_TILE_WIDTH + threadIdx.x < rows_B && col < cols_B) ? d_B[(i * SHARED_TILE_WIDTH + threadIdx.x) * cols_B + col] : 0.0;
 
 		__syncthreads();
 		for (int j = 0; j < SHARED_TILE_WIDTH; j++)
 		{
-			c_val += s_A[threadIdx.y][j] * s_B[j][threadIdx.x];
+			c_val += s_A[threadIdx.x][j] * s_B[j][threadIdx.y];
 		}
 		__syncthreads();
 	}
