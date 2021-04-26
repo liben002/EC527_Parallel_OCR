8c17ff92c078b47ce19d070e9a5d1b564c2dbc12
Panat Taranat <ptaranat@scc1.bu.edu>
2021-04-26 01:43:53 -0400

    Add 8core results

--- job.sh
+++ job.sh
@@ -2,9 +2,10 @@
 
 #$ -P ec527
 
-#$ -pe omp 8
+#$ -pe omp 4
 
 module load gcc
 cd /usr4/ec526/ptaranat/ec527/EC527_Parallel_OCR || exit
 make
 OMP_NUM_THREADS=$NSLOTS /usr4/ec526/ptaranat/ec527/EC527_Parallel_OCR/parallel
+
