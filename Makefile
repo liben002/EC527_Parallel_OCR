577e94a5c0ce176bf62d1a3ead23c4ece083c858
Panat Taranat <ptaranat@scc1.bu.edu>
2021-04-26 02:31:04 -0400

    Add avx

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: serial parallel
 
 serial:
-	g++ main.cpp -O3 -std=c++11 -lrt -lm -o serial
+	g++ main.cpp -O3 -std=c++11 -lrt -lm -o serial -use-intel-optimized-headers
 
 parallel:
 	g++ main.cpp -O3 -std=c++11 -fopenmp -lrt -lm -o parallel
--- job.sh
+++ job.sh
@@ -2,10 +2,10 @@
 
 #$ -P ec527
 
-#$ -pe omp 4
+#$ -l avx
 
 module load gcc
 cd /usr4/ec526/ptaranat/ec527/EC527_Parallel_OCR || exit
 make
-OMP_NUM_THREADS=$NSLOTS /usr4/ec526/ptaranat/ec527/EC527_Parallel_OCR/parallel
-
+#OMP_NUM_THREADS=$NSLOTS /usr4/ec526/ptaranat/ec527/EC527_Parallel_OCR/parallel
+./serial
